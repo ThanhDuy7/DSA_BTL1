53fe78717ac0841dbe549fe61699b56ecca2bf45
ThanhDuy7 <thanh.daoeth3real07@hcmut.edu.vn>
2023-10-07 23:19:29 +0700

    add count, countQueue,current customer and destructor

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -2,17 +2,61 @@
 
 class imp_res : public Restaurant
 {
+	public:
+	int count;
+	int countQueue;
+	customer* current;
 	public:	
 		imp_res() {};
-
+		imp_res() : count(0), countQueue(0), current(NULL) {};
+		~imp_res() {
+			if (this->current != NULL) {
+				customer *temp = this->current->next;
+				while (temp != this->current) {
+					customer *temp2 = temp;
+					temp = temp->next;
+					delete temp2;
+				}
+				delete this->current;
+				count = 0;
+				current = NULL;
+			} 
+		};
 		void RED(string name, int energy)
 		{
 			cout << name << " " << energy << endl;
+
+			//base case
+			if (count != 0) 
+			{
+				customer *temp = this->current;
+				for (int i = 0; i < count; i++) {
+					if (temp->name == name) return;
+					temp = temp->next;
+				}
+			}
+			
+			if (energy == 0 || countQueue == MAXSIZE) return;
+
 			customer *cus = new customer (name, energy, nullptr, nullptr);
+			if (this->count < MAXSIZE/2) 
+			{
+				this->count++;
+				if (this->count == 1) {
+					this->current = cus;
+					return;
+				}
+
+			}
+			
+			
+			
+			
+
+	
 		}
-		void BLUE(int num)
-		{
-			cout << "blue "<< num << endl;
+		void BLUE(int num) {
+			cout << "blue " << num << endl;
 		}
 		void PURPLE()
 		{
